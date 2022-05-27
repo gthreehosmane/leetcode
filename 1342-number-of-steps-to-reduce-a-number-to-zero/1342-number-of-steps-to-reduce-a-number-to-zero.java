3240a5ece0bdf71457e7710b6545bbbd550802fb
Teja Ganapati Jaddipal(SJSU ID- 015957526)) <teja.jaddipal@gmail.com>
2022-05-27 11:32:37 -0700

    Time: 1 ms (54.47%), Space: 40.9 MB (55.29%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int temp = num;
+        int count = 0;
+        while(temp!=0){
+            if(temp%2 !=0){
+                temp=temp-1;
+                count++;
+            }
+            else{
+            temp=temp/2;
+            count++;
+            }
+        }
+        return count;
+        
+    }
+}
