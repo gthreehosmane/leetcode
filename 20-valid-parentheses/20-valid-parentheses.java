d7b66a4ba338b38b283fc3767c51abe3b84666d2
Teja Ganapati Jaddipal(SJSU ID- 015957526)) <teja.jaddipal@gmail.com>
2022-05-30 21:36:25 -0700

    Time: 4 ms (22.14%), Space: 42.1 MB (38.82%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(')
+                st.push(')');
+            else if(c == '{')
+                st.push('}');
+            else if(c == '[')
+                st.push(']');
+            else if(st.empty() || c != st.pop())
+                return false;
+        }
+        return st.isEmpty();
+    }
+}
