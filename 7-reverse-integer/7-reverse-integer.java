e4acde8493302847c24e763f8356629dfe2413c6
Teja Ganapati Jaddipal(SJSU ID- 015957526)) <teja.jaddipal@gmail.com>
2022-06-01 20:01:27 -0700

    Time: 6 ms (10.67%), Space: 41.5 MB (44.63%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,13 +1,11 @@
 class Solution {
     public int reverse(int x) {
-        int rev = 0;
-        while(x!=0){
-            int rem = x%10;
-            x=x/10;
-            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && rem > 7)) return 0;
-            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && rem < -8)) return 0;
-            rev = rev*10+rem;
+        String s = new StringBuffer().append(Math.abs(x)).reverse().toString();
+        try{
+        return (x<0) ? Integer.parseInt(s) * -1: Integer.parseInt(s);
         }
-        return rev;
+        catch(NumberFormatException e){
+            return 0;
+        }
     }
 }
