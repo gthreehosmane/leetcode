d2c37b60ef85d0d9cd8cbc33fc795920a38ad06b
Teja Ganapati Jaddipal(SJSU ID- 015957526)) <teja.jaddipal@gmail.com>
2022-09-18 20:04:55 -0700

    Time: 1 ms (97.73%), Space: 54 MB (56.67%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,6 +1,6 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> rows = new HashSet<>();
+       /* Set<Integer> rows = new HashSet<>();
         Set<Integer> cols = new HashSet<>();
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[i].length;j++){
@@ -15,6 +15,38 @@ class Solution {
                 if(rows.contains(i) || cols.contains(j)){
                     matrix[i][j] = 0;
                 }
+            }
+        }*/
+        Boolean isCol = false;
+        
+        for(int i=0;i<matrix.length;i++){
+                if(matrix[i][0] == 0){
+                    isCol = true;
+                }
+                for(int j=1;j<matrix[i].length;j++){
+                    if(matrix[i][j] == 0){
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
+                }
+        }
+        
+        
+        for(int i=1;i<matrix.length;i++){
+                for(int j=1;j<matrix[i].length;j++){
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                        matrix[i][j] = 0;
+                    }
+                }
+        }
+        if(matrix[0][0] == 0){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(isCol){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][0] = 0;
             }
         }
     }
